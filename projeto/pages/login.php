44d912b7dae6f981f5aad168c8100e05d87789bf
Equipe25percentprojeto2 <projetointegradodois@gmail.com>
2024-08-30 09:30:05 -0300

    Update login.php
    Login na metade do caminho pra funcionar, banco de dados já funciona mas ainda falta colocar um cadastro teste, que por alguma razão não quer dar certo.

--- projeto/pages/login.php
+++ projeto/pages/login.php
@@ -46,45 +46,70 @@
     </div>
   </div>
 <?php
-   // include('conexao.php');
-    if (isset($_POST['salvar']) && isset($_POST['email']) && isset($_POST['pass'])){
-    
-      $email= mysqli_real_escape_string($mysqli,$_POST['email']);
-      $senha= mysqli_real_escape_string($mysqli,$_POST['pass']);
-      $cripto= password_hash($senha, PASSWORD_DEFAULT);
-  
-      //VALIDAÇÃO DE CAMPO VAZIO
-      if ($matri=="" || $matri==null){
-          echo "'Úsuario não pode ser vazio.";
-          exit();
-      }
-  
-      if ($email=="" || $email==null){
-          echo "Email não pode ser vazio.";
-          exit();
-      }
-  
-      if ($senha=="" || $senha==null){
-          echo "Senha não pode ser vazia.";
-          exit();
-      }
-      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-          echo "<b style='color:red'>Formato de email inválido!</b>";
-          exit();
-      }
-      $result = $mysqli->query("SELECT COUNT(*) FROM user_com WHERE matri = '{$matri}'");
-      $result2 = $mysqli->query("SELECT COUNT(*) FROM user_com WHERE email = '{$email}'");
-      $row = $result->fetch_row();
-      $row2 = $result2->fetch_row();
-      if ($row[0] > 0 || $row2[0] > 0) {
-      echo "<b style='color:red'>Esse usuário já existe.</b>";
-      } else {
-        $sql = $mysqli->prepare("INSERT INTO user_com(matri,email,pass) VALUES ('$matri','$email','$cripto')");
-        $sql->execute();
-        echo "<b style='color:green'>Úsuario inserido com sucesso!</b>";
-      
-      }
-    }
+    include('conexao.php');
+    $adm = false;
+    if(empty(!empty($_POST) && $_POST['email']) || empty($_POST['pass'])){
+    //    header('Location: index.php');
+        exit();
+    } 
+
+    $email = mysqli_real_escape_string($mysqli,$_POST['email']);
+    $senha = mysqli_real_escape_string($mysqli,$_POST['pass']);
+    $cripto= password_hash($senha, PASSWORD_DEFAULT);
+    echo "<b style= 'color:red'>$cripto</b>";
+        if ($email=="" || $email==null){
+            echo "Email não pode ser vazio.";
+            exit();
+        }
+
+        if ($senha=="" || $senha==null){
+            echo "Senha não pode ser vazia.";
+            exit();
+        }
+        $sql_code = "SELECT emailaluno, pass FROM alunos WHERE email = '$email'";
+        
+        $result = mysqli_query($mysqli, $sql_code);
+        
+        $quant = mysqli_num_rows($result);
+        
+        $sql_exec = mysqli_query($mysqli,$sql_code) or die($mysqli->error);
+        
+        $user = $sql_exec->fetch_assoc();
+        if(password_verify($senha, $user['pass'])){
+            if($quant == 1) {
+                if(!isset($_SESSION)) {
+                    session_start();
+                    $_SESSION['email'] = $email;}
+                    header("Location: principal.php");
+                    exit();
+
+            }} else {
+            $adm = true;
+        }if($adm==true){
+            $sql_code_adm = "SELECT email_inst,pass FROM user_adm WHERE email_inst = '$email' AND pass = '$senha'";
+            $result_adm = mysqli_query($mysqli, $sql_code_adm);
+            $sql_exec_adm = mysqli_query($mysqli,$sql_code_adm) or die($mysqli->error);
+            $user_adm = $sql_exec_adm->fetch_assoc();
+            $quant = mysqli_num_rows($result_adm);
+            if($quant == 1) {
+                if(!isset($_SESSION)) {
+                session_start();
+                $_SESSION['email_inst'] = $email;}
+                header("Location: principalADM.php");
+                exit();
+        
+            } else {
+                echo "<b style= 'color:red'>Falha ao logar! E-mail ou senha incorretos</b>";
+                exit();
+                header("Location: index.html");
+                
+                
+            }}else{
+            echo "Falha ao logar, usuário ou email incorretos.";
+              }
+        
+
+
 ?>
 </body>
 
